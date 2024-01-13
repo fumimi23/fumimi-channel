38847442da64f2a16a82ae72f1459645bae11fd2
fumimi23 <fumio.kogure@glucose.jp>
2024-01-14 00:52:32 +0900

    feat: post用テーブル追加

--- back-end/app/model.py
+++ back-end/app/model.py
@@ -11,6 +11,18 @@ class ThreadTable(Base):
     title = Column(String(120), nullable=False)
 
 
+# Postテーブル定義
+class PostTable(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(60), nullable=False)
+    email = Column(String(60), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    displayed_id = Column(String(9), default="aaaa", nullable=False)
+    thread_id = Column(Integer, ForeignKey("thread.id"), nullable=False)
+    message = Column(String(1000), nullable=False)
+
+
 def main():
     # テーブル構築
     Base.metadata.create_all(bind=ENGINE)
--- back-end/initdb.d/create_table.sql
+++ back-end/initdb.d/create_table.sql
@@ -4,3 +4,14 @@ CREATE TABLE thread (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE post (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    displayed_id VARCHAR(9) NOT NULL,
+    thread_id INT NOT NULL,
+    message VARCHAR(1000) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (thread_id) REFERENCES thread(id)
+);
