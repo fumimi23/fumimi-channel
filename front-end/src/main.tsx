ac13dc1c6b240291262cedae89b36d7db2537d76
fumimi23 <fumio.kogure@glucose.jp>
2024-01-14 15:40:53 +0900

    feat: set up routing

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import routes from "~react-pages";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: routes,
+  },
+]);
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => {
+    router.dispose();
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
