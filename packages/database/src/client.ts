ea234c1769e79da8141e4be15b5259bdb044ff34
fumimi23 <highspec.nite@gmail.com>
2026-01-01 14:08:35 +0900

    Revert "fix: prisma error"
    This reverts commit 4d8aad786c2cf737bda691db1786f04f83432072.

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,12 +1,6 @@
 import {PrismaClient} from '@prisma/client';
-import {PrismaPg} from '@prisma/adapter-pg';
-import pg from 'pg';
 
 export function createPrismaClient(): PrismaClient {
-	// Prisma 7では、アダプターまたはAccelerateを使用
-	const pool = new pg.Pool({connectionString: process.env.DATABASE_URL});
-	const adapter = new PrismaPg(pool);
-
-	const prismaClient = new PrismaClient({adapter});
+	const prismaClient = new PrismaClient();
 	return prismaClient;
 }
